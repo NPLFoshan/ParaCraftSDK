d0eb352902fc19d422d7d2ed6cb972ab23ff1da8
onedou <onedou@126.com>
2021-09-28 16:05:17 +0800

    fixed bugs

--- bin/DevWithHttpDebug.bat
+++ bin/DevWithHttpDebug.bat
@@ -2,6 +2,6 @@
 
 pushd "%~dp0../../redist/" 
 
-call "ParaEngineClient.exe" mod="WorldShare|ExplorerApp|DiffWorld" loadpackage="%~dp0../../trunk/,;%~dp0./WorldShare/,;%~dp0./ExplorerApp/,;%~dp0./DiffWorld/" single="false" mc="true" noupdate="true" isDevEnv="true" httpdebug="true"
+call "ParaEngineClient.exe" mod="WorldShare|ExplorerApp|DiffWorld" loadpackage="%~dp0../../trunk/,;%~dp0../_mod/WorldShare/,;%~dp0../_mod/ExplorerApp/,;%~dp0../_mod/DiffWorld/" single="false" mc="true" noupdate="true" isDevEnv="true" httpdebug="true"
 
 popd 
