9d0e6d24c8b24fc0fce3947b2502e7cce96cfb4f
onedou <onedou@126.com>
2021-10-25 16:36:30 +0800

    updated UpgradeMod.bat

--- bin/UpgradeMod.bat
+++ bin/UpgradeMod.bat
@@ -1,10 +1,13 @@
 @echo off 
 
-set Root="%~dp0../_mod/"
+echo Upgrade mod starting...
+
+set root=%~dp0
 
 call :UpdateMod WorldShare
 call :UpdateMod ExplorerApp
 call :UpdateMod DiffWorld
+call :UpdateMod trunk
 
 popd
 
@@ -12,9 +15,16 @@ EXIT /B %ERRORLEVEL%
 
 rem update mod git
 :UpdateMod
-echo %1
-pushd %Root%%1
-git pull --rebase
-popd
+	echo %1
+
+	if "%1" == "trunk" (
+		pushd "%root%../../%1"
+		svn update .
+		popd
+	) else (
+		pushd "%root%../_mod/%1"
+		git pull --rebase
+		popd
+	)
 
 EXIT /B 0
