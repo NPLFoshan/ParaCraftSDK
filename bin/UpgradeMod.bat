96bbd15859cbcb0d151056f48d2d6566e10e3e83
onedus <onedous@gmail.com>
2021-11-03 11:53:16 +0800

    updated UpgradeMod.bat

--- bin/UpgradeMod.bat
+++ bin/UpgradeMod.bat
@@ -9,22 +9,34 @@ call :UpdateMod ExplorerApp
 call :UpdateMod DiffWorld
 call :UpdateMod trunk
 
-popd
+exit /b %errorlevel%
 
-EXIT /B %ERRORLEVEL%
-
-rem update mod git
+@rem update mod git and svn
 :UpdateMod
-	echo %1
+	set param1=%1
+	echo %param1%
 
-	if "%1" == "trunk" (
-		pushd "%root%../../%1"
-		svn update .
-		popd
+	if "%param1%" == "trunk" (
+		set cur_path="%root%../../%param1%"
 	) else (
-		pushd "%root%../_mod/%1"
-		git pull --rebase
-		popd
+		set cur_path="%root%../_mod/%param1%"
 	)
 
-EXIT /B 0
+	if "%param1%" == "trunk" (
+		if exist %cur_path% (
+			pushd %cur_path%
+			svn update .
+			popd
+		) else (
+			svn co svn://svn.paraengine.com/script/%param1% %cur_path%
+		)
+	) else (
+		if exist %cur_path% (
+			pushd %cur_path%
+			git pull --rebase
+			popd
+		) else (
+			git clone git@github.com:tatfook/%param1%.git %cur_path%
+		)
+	)
+exit /b %errorlevel%
